f2263bd685f3b3f40226f783e2b04f8c1a822ef3
philipfleischer <fleischer1@live.no>
2026-01-12 09:21:16 +0100

    Developed the HTML structure using svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import TodoItem from '$lib/todo-item.svelte';
+
+  // Now Changing this changes it every title place in the code!
+  const title = 'Todo';
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
+
+<h1>{title}</h1>
+<form action="" method="">
+  <input type="text" name="text" aria-label="Add a todo" placeholder="+ type to add a todo" />
+</form>
+
+<!-- import todo.items in Svelte, $lib ensures we can copypaste the relative path anywhere-->
+<TodoItem />
